cbc43fb3e59aed47b49bdc7c471e00bd56707c3e
SharukAli <83536265+SharukAli@users.noreply.github.com>
2021-05-17 18:02:02 +0530

    Add files via upload

---
+++ cucumber/GoogleSearchSteps.java
@@ -0,0 +1,17 @@
+package stepDefinition;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class GoogleSearchSteps {
+
+	@Given("^ Statement set in Feature file for Given $")
+	public void givenFunctionName() {}
+
+	@When("^ Statement set in Feature file for When $")
+	public void whenFunctionName() {}
+
+	@Then("^ Statement set in Feature file for Then $")
+	public void thenFunctionName() {}
+}
